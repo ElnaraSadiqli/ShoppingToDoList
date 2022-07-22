9f5e7034151955095428998f8ff8f56efb6174ba
ElnaraSadiqli <elnaragk@code.edu.az>
2022-07-22 20:19:51 +0400

    "Inserted new data to array"

--- script.js
+++ script.js
@@ -6,101 +6,177 @@ let inputCode = document.querySelector(".input-code");
 let trr = document.getElementsByTagName("tr");
 // Button
 let btn = document.querySelector("#btn-submit");
-let delBtn = document.querySelector(".delete-btn");
-let upBtn = document.querySelector(".update-btn");
 
 // Table
 let table = document.querySelector(".table");
 let ttbody = document.querySelector(".table-body");
 let tr;
-
+let data;
 let bodyTable = [];
 
 
+"use strict";
 eventListeners();
-AddData();
 
 function eventListeners() {
-    btn.addEventListener("click", pushData);
 
-    btn.addEventListener("keypress", function (event) {
+    btn.addEventListener("click", addNewItem);
 
-        if (event.key == "Enter") {
-            document.querySelector("#btn-submit").click();
-        }
 
-        event.preventDefault();
-    })
+
 }
 
-
-
-
-function AddData(event) {
-
-
-    for (let data of bodyTable) {
-
-        tr = `
-        <th scope="row">${data.id}</th>
-    <td>${inputName.value}</td>
-    <td>${inputCount.value}</td>
-    <td>${inputCode.value}</td>
-    <div class="dropdown">
-  <a class="btn btn-slink dropdown-toggle btn-danger" style="color:black" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-  ...
-  </a>
-
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <li><a class="dropdown-item delete-btn" onclick="deleteTask(${data.id})" href="#" >Delete</a></li>
-    <li><a class="dropdown-item update-btn"  href="#" >Update</a></li>
-  </ul>
-</div>`
-
-        ttbody.insertAdjacentHTML("beforeend", tr);
-
-
-
-        data.id++;
-        event.preventDefault();
-    }
-
-
-    event.preventDefault();
-}
-
-function pushData(event) {
-
+function addNewItem(event) {
 
     bodyTable.push({
+
         "id": bodyTable.length + 1,
         "name": inputName.value,
-        "code": inputCode.value,
         "count": inputCount.value,
-        "price": inputPrice.value
+        "price": inputPrice.value,
+        "barcode": inputCode.value
+
     })
 
+    creatItem();
 
 
-    AddData();
+    event.preventDefault();
+};
+
+
+function creatItem(event) {
+  
+    ttbody.innerHTML = "";
+
+
+    for (data of bodyTable) {
+
+
+
+     tr = `
+        <th scope="row">${data.id}</th>
+        <td>${data.name}</td>
+        <td>${data.count}</td>
+        <td>${data.price}</td>
+        <td>${data.barcode}</td>
+      
+    <div class="dropdown">
+     <a class="btn btn-slink dropdown-toggle btn-danger" style="color:black" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+    ...
+     </a>
+
+     <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+     <li><a class="dropdown-item delete-btn" onclick="deleteTask()" href="#" >Delete</a></li>
+     <li><a class="dropdown-item update-btn"  onclick='editTask()' href="#" >Update</a></li>
+     </ul>
+     </div>  `
+
+     ttbody.insertAdjacentHTML("beforeend", tr)
+
+     }
+
+
     event.preventDefault();
 }
 
 
-function deleteTask(item) {
-    let deleteId;
+console.log(ttbody);
 
-    for (let i in bodyTable) {
+// error table artmir
 
-        if (bodyTable[i].id == item) {
+// function eventListeners() {
+//     btn.addEventListener("click", pushDataArray);
 
-            deleteId = i;
-        }
+//     btn.addEventListener("keypress", function (event) {
 
-    }
+//         if (event.key == "Enter") {
+//             document.querySelector("#btn-submit").click();
+//         }
 
-    bodyTable.splice(deleteId, 1);
+//         event.preventDefault();
+//     })
+// }
 
 
 
-}
+// function addData(event) {
+
+//     ttbody.innerHTML = "";
+
+
+//     for (let data of bodyTable) {
+
+//         tr = `
+//     <th scope="row">${bodyTable.length }</th>
+//     <td>${inputName.value}</td>
+//     <td>${inputCount.value}</td>
+//     <td>${inputPrice.value}</td>
+//     <td>${inputCode.value}</td>
+//     <div class="dropdown">
+//      <a class="btn btn-slink dropdown-toggle btn-danger" style="color:black" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+//      ...
+//      </a>
+
+//     <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+//     <li><a class="dropdown-item delete-btn" onclick="deleteTask(${data.id})" href="#" >Delete</a></li>
+//     <li><a class="dropdown-item update-btn"  onclick='editTask(${data.id},"${data.name}")' href="#" >Update</a></li>
+//     </ul>
+//     </div>`;
+
+//         ttbody.insertAdjacentHTML("beforeend", tr);
+
+
+
+//     }
+
+
+
+//     event.preventDefault();
+// }
+// console.log(addData(e));
+
+// function pushDataArray(event) {
+
+//     if (inputName.value == "") {
+//         alert("Insert text !!")
+//     } else {
+
+//         bodyTable.push({
+//             "id": bodyTable.length + 1,
+//             "name": inputName.value,
+//             "code": inputCode.value,
+//             "count": inputCount.value,
+//             "price": inputPrice.value
+//         });
+//         bodyTable.id++;
+
+//         addData();
+
+//     }
+
+//     event.preventDefault();
+
+
+// }
+
+
+
+// function deleteTask(iD) {
+
+//     let deleteD;
+
+//     deleteD = bodyTable.findIndex((item) => item.id == iD);
+
+
+//     bodyTable.splice(deleteD, 1);
+
+//     addData();
+// }
+
+
+// function editTask(ids) {
+//     editId = ids;
+
+
+// }
